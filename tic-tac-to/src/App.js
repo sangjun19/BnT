ffe616fd98fd6b86f0dcd0c4e5060e061aa47fcd
sangjun <sangjun0319@gmail.com>
2024-06-24 15:47:10 +0900

    tictacto

---
+++ tic-tac-to/src/App.js
@@ -0,0 +1,152 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    const prevSquares = squares;
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    if (squares[i]) {
+      return;
+      const temp = nextSquares[i];
+      nextSquares[i] = null;
+      console.log(squares.lenght);
+      if (prevSquares != null && arraysEqual(prevSquares, nextSquares)) {
+        xIsNext = !xIsNext;
+      } else {
+        return;
+      }
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    if (nextMove === -1) {
+      setHistory([Array(9).fill(null)]);
+      setCurrentMove(0);
+    } else {
+      setCurrentMove(nextMove);
+    }
+  }
+
+  function backClick() {
+    if (currentMove > 0) {
+      setCurrentMove(currentMove - 1);
+      setHistory((prevHistory) => prevHistory.slice(0, -1));
+    }
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (calculateWinner(squares)) {
+      description = "Go to game restart";
+      move = -1;
+    } else if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <div>
+          <button className="GoBack" onClick={backClick}>
+            Go Back
+          </button>
+        </div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function arraysEqual(arr1, arr2) {
+  return arr1.every((element, index) => element === arr2[index]);
+}
