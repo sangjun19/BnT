e6e586cea3196b8f7082fa1068fa541d93b980fc
sangjun <sangjun0319@gmail.com>
2024-08-01 16:09:21 +0900

    08011609

---
+++ coding_test/피보나치 수.py
@@ -0,0 +1,15 @@
+def fibo(n):
+    sum = 0
+    arr = [0, 1, 1]        
+    for i in range(3, n + 1):
+        arr.append(arr[i - 1] + arr[i - 2])
+        
+    print(arr)
+    return arr[n]
+
+def main():
+    n = int(input())
+    print(fibo(n))
+    
+if __name__ == "__main__":
+    main()
