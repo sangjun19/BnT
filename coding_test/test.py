58116094d2c35a05e25d03c6b5636f70360a3bb3
sangjun <sangjun0319@gmail.com>
2024-08-02 16:42:55 +0900

    08021642

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,24 +1,26 @@
-result = 0
-
-def back(arr, cnt, sum, answer):
-    global result
-    if cnt == len(arr):
-        if sum == answer:
-            result += 1
-        return
-    for i in range(len(arr[cnt])):
-        if sum + arr[cnt][i] <= answer:
-            back(arr, cnt + 1, sum + arr[cnt][i], answer)
-
 def main():
     n, m, k = map(int, input().split())
     arr = []
     for _ in range(n):
         temp = list(map(int, input().split()))
-        temp[0] = 0
-        arr.append(temp)
-    back(arr, 0, 0, k)
-    print(result)
-
+        arr.append(temp[1:])
+    
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    dp[0][0] = 1
+    
+    for i in range(1, n + 1):
+        coins = arr[i - 1]
+        
+        for j in range(k + 1):
+            dp[i][j] = dp[i - 1][j]
+        
+        for coin in coins:
+            for j in range(k, coin - 1, -1):
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - coin]) % 10007
+                
+    for d in dp:
+        print(d)
+    print(dp[n][k])
+    
 if __name__ == "__main__":
     main()
