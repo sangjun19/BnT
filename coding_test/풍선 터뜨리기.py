bcc0fd214d355fc8d99ddb991c2678f95693c5e5
sangjun <sangjun0319@gmail.com>
2024-08-12 16:36:05 +0900

    08121636

--- coding_test/풍선 터뜨리기.py
+++ coding_test/풍선 터뜨리기.py
@@ -1,11 +1,28 @@
-n = int(input())
-arr = list(map(int, input().split()))
-num = [i for i in range(1, n + 1)]
-idx = 0
+def balloon_game(N, balloon_values):
+    balloons = list(enumerate(balloon_values, start=1))  # (번호, 값) 쌍의 리스트
+    result = []
+    idx = 0  # 시작 인덱스
 
-for i in range(n):
-    print(num[idx], end=" ")
-    num.pop(idx)
-    idx = (idx + arr[idx] + len(arr)) % len(arr)
-    arr.pop(idx)
-    
+    while balloons:
+        # 현재 풍선의 번호를 결과에 추가
+        num, move = balloons.pop(idx)
+        result.append(num)
+        
+        if not balloons:
+            break
+        
+        # 다음 인덱스 계산
+        if move > 0:
+            idx = (idx + (move - 1)) % len(balloons)
+        else:
+            idx = (idx + move) % len(balloons)
+
+    return result
+
+# 입력 받기
+N = int(input())
+balloon_values = list(map(int, input().split()))
+
+# 결과 출력
+result = balloon_game(N, balloon_values)
+print(" ".join(map(str, result)))
