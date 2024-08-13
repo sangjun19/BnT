72abca1516f47b987ddff32602f8fb4cded27332
sangjun <sangjun0319@gmail.com>
2024-08-13 16:20:12 +0900

    08131620

---
+++ coding_test/인간-컴퓨터 상호작용.py
@@ -0,0 +1,12 @@
+s = input()
+q = int(input())
+arr = []
+for _ in range(q):
+    arr.append(input().split(" "))
+
+for i in range(q):
+    cnt = 0
+    for k in range(int(arr[i][1]), int(arr[i][2]) + 1):
+        if s[k] == arr[i][0]:
+            cnt += 1
+    print(cnt)
