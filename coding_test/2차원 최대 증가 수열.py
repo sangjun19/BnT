bf762174e71d7163c0e43f7fcb460ceed555d960
sangjun <sangjun0319@gmail.com>
2024-08-16 16:02:55 +0900

    08161602

---
+++ coding_test/2차원 최대 증가 수열.py
@@ -0,0 +1,21 @@
+def main():
+    n, m = map(int, input().split())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    dp = [[0] * m for _ in range(n)]
+    dp[0][0] = 1
+    for i in range(1, n):
+        for j in range(1, m):
+            for y in range(0, i):
+                for x in range(0, j):
+                    if dp[y][x] == 0:
+                        continue
+                    if arr[y][x] < arr[i][j]:
+                        dp[i][j] = max(dp[i][j], dp[y][x] + 1)
+    for d in dp:
+        print(*d)
+    print(max(map(max, dp)))
+    
+if __name__ == "__main__":
+    main()
